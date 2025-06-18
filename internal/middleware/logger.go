46869009d7e863765fa298a4af722d2c63573ef3
Aoi <metsk.150@gmail.com>
2025-06-18 20:21:46 +0900

    add file in internal/middleware

---
+++ internal/middleware/logger.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+
+		// Process request
+		c.Next()
+
+		// Log request details
+		latency := time.Since(start)
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+
+		if raw != "" {
+			path = path + "?" + raw
+		}
+
+		log.Info().
+			Str("client_ip", clientIP).
+			Str("method", method).
+			Str("path", path).
+			Int("status", statusCode).
+			Dur("latency", latency).
+			Msg("request processed")
+	}
+}
